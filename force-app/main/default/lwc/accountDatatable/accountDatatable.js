2b976d843c99f32f17e59ed4dbe2293844d58f1d
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-08-05 11:12:40 +0530

    picklist updating

--- force-app/main/default/lwc/accountDatatable/accountDatatable.js
+++ force-app/main/default/lwc/accountDatatable/accountDatatable.js
@@ -1,31 +1,22 @@
 /**
- * @File Name          : accountDataTable.js
- * @Description        :
- * @Author             :
- * @Group              :
- * @Last Modified By   : Vrushabh Uprikar
- * @Last Modified On   : 04-08-2021
- * @Modification Log   :
- * Ver       Date            Author      		    Modification
- * 1.0    (DD-MM-YYYY)                               Initial Version
- **/
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-08-2021
+ * @last modified by  : Vrushabh Uprikar
+**/
 import { LightningElement, wire, track, api } from 'lwc';
-
 import { reduceErrors } from 'c/ldsUtils';
 import getFieldSetAndRecords from '@salesforce/apex/picklistDatatableEditContoller.getFieldSetAndRecords';
 import upsertSOBJRecord from '@salesforce/apex/picklistDatatableEditContoller.upsertSOBJRecord';
-
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
+import updateAccounts from '@salesforce/apex/picklistDatatableEditContoller.updateAccounts';
 //  const column=[
 //      { "label" : "Name", "apiName" : "Name" ,"fieldType":"text","objectName":"Account", "fieldName": "Name", editable: true },
 //      { "label" : "Phone", "apiName" : "Phone" ,"fieldType":"text","type":"phone","objectName":"Account","fieldName": "Phone", editable: true },
 //      { "label" : "Total Spending", "apiName" : "Total_Spending__c" ,"fieldType":"text","objectName":"Account","fieldName": "Total_Spending__c", editable: true },
 //  ];
-
 export default class AccountDatatable extends LightningElement {
-
+    @api isLoaded = false;
     @track allData = []; // Datatable
     allDataOrgCopy = []; // DatatableOrignalCpy
     @track columns = [];
@@ -34,22 +25,17 @@ export default class AccountDatatable extends LightningElement {
     @track fieldOptionJSON = []; // list of fields option for combobox
     @track FieldsValue; // default value of combobox
     @track fieldType; // data type
-
     @api SFDCobjectApiName;
     @api fieldSetName;
-
     fieldName;
     listOfFieldsCopy = [];
-
+    draftValues = [];
     /*records=[];
     wiredRecords;
     error;
-
     keyIndex = 0;
-    draftValues = [];
     picklistval;
     updatedFields=[]
-
     //Json to insert new row in Datatable
     acc = {
         Id: 0,
@@ -58,9 +44,8 @@ export default class AccountDatatable extends LightningElement {
         Fax: "",
         Industry: "",
     }*/
-
-
     connectedCallback() {
+
         getFieldSetAndRecords({
             strObjectApiName: this.SFDCobjectApiName,
             strfieldSetName: this.fieldSetName
@@ -68,14 +53,11 @@ export default class AccountDatatable extends LightningElement {
             .then(async data => {
                 this.isLoading = true;
                 let objStr = JSON.parse(data);
-
                 let listOfFields = JSON.parse(Object.values(objStr)[2]);
                 this.listOfFieldsCopy = listOfFields;
                 console.log('listOfFields:', JSON.stringify(listOfFields)); // Fields
-
                 var listOfRecords = JSON.parse(Object.values(objStr)[1]);
                 console.log('listOfRecords:', JSON.stringify(listOfRecords)); // Data
-
                 var pickListValues = JSON.parse(Object.values(objStr)[0]);
                 console.log('pickListValues:', JSON.stringify(pickListValues)); // Picklist
                 var pickListArray = [];
@@ -88,9 +70,8 @@ export default class AccountDatatable extends LightningElement {
                 var xx = JSON.stringify(listOfRecords);
                 this.allData = JSON.parse(xx);
                 this.allDataOrgCopy = JSON.parse(xx);
-
                 await listOfFields.map(element => {
-
+                    this.isLoading = true;
                     if (element.type == 'picklist' || element.type == "picklist") {
                         let opt = []; // options
                         pickListValues.forEach(pic => {
@@ -105,7 +86,6 @@ export default class AccountDatatable extends LightningElement {
                                 }
                             });
                         });
-
                         let colJson =
                         {
                             fieldName: element.fieldPath,
@@ -131,7 +111,6 @@ export default class AccountDatatable extends LightningElement {
                         };
                         this.columns.push(colJson);
                         console.log('colJson:', colJson);
-
                     } else {
                         let elm = {
                             label: element.label,
@@ -146,7 +125,6 @@ export default class AccountDatatable extends LightningElement {
                         console.log('elm:', elm);
                     }
                 });
-
             })
             .then(_ => {
                 var parseCol = JSON.stringify(this.columns);
@@ -161,24 +139,23 @@ export default class AccountDatatable extends LightningElement {
                 this.allData = undefined;
             });
     }
-
+    // async call for piclist
     // Picklist change code start Here
     picklistChanged(event)
     {
         try {
             // collect values of piclist and update it to draft value
             let dataRecieved = JSON.parse(JSON.stringify(event.detail.data));
-            console.log('Picklist Change Data : 1', dataRecieved);
-            let fieldApiName = dataRecieved.apiname;
-            let ObjectIndex;
-            ObjectIndex = this.records.findIndex(Item => { return Item.Id === dataRecieved.context });
-            console.log(ObjectIndex);
-
+            console.log('Picklist Change Data : ', dataRecieved);
+            let picKlistdata = {
+                Id:dataRecieved.context,
+                [dataRecieved.apiname]:dataRecieved.value
+            };
+            this.draftValues.push(picKlistdata);
         } catch (error) {
             console.log(error);
         }
     }
-
     updateDraftValues(updateItem) {
         // let records = JSON.parse(JSON.stringify(this.records));
         let findObjIndex = this.records.findIndex((arrItem, ind) => {
@@ -193,57 +170,46 @@ export default class AccountDatatable extends LightningElement {
             let includes = false;
             this.draftValues = updateItem
             console.log(this.draftValues);
-
         }
     }
-
     // Handlsave event to save Edit draftvalues and Record Insert save button
-    async handleSave(event)
-    {
+    handleSave(event) {
         var updatedField = event.detail.draftValues;
-        // const updatedField2 = this.updatedFields;
+        console.log('draft val on handelSave' + JSON.stringify(event.detail.draftValues));
         console.log('updatedField' + JSON.stringify(updatedField));
-        await upsertSOBJRecord({ jSONSObject: JSON.stringify(updatedField), sObjectApiName: this.SFDCobjectApiName })
-            .then(result => {
-
-                console.log(JSON.stringify("Apex update result: " + result));
+        this.getAlldata(updatedField);
+        //const recordInput = {updatedField};
+        updateAccounts({ data: updatedField })
+            .then(() => {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Success',
-                        message: 'Account(s) updated',
+                        message: 'Account updated',
                         variant: 'success'
                     })
                 );
-
-                refreshApex(this.wiredRecords).then(() => {
-                    this.draftValues = [];
-                });
-
+                // Display fresh data in the datatable
+                // return refreshApex(this.contact).then(() => {​
+                //     // Clear all draft values in the datatable
+                //     this.draftValues = [];// });
             }).catch(error => {
-
-                console.log('Error is ' + JSON.stringify(error));
                 this.dispatchEvent(
                     new ShowToastEvent({
-                        title: 'Error updating or refreshing records',
+                        title: 'Error updating or reloading record',
                         message: error.body.message,
                         variant: 'error'
                     })
                 );
-
             });
-
-
-        // KeyIndex equles to zero
-
-        this.acc.Id = this.keyIndex;
-        this.keyIndex = 0;
-        console.log(this.keyIndex);
+    }
+    handleCellChange(event)
+    {
+        var All_Draft=[];
+        console.log('draft val nnn' + JSON.stringify(event.detail.draftValues));
+        let draft_val = event.detail.draftValues;
 
     }
-    handleCellChange(event) {
-        console.log('draft val @oncellchange' + JSON.stringify(event.detail.draftValues));
-    }
-    // Add row button logic
+
     addRow() {
         console.log("add row function called");
         var dynamicArray = this.listOfFieldsCopy;
@@ -256,12 +222,10 @@ export default class AccountDatatable extends LightningElement {
                 type: this.SFDCobjectApiName,
                 url: "",
             },
-
         };
         // dynamicArray.forEach(obj=>{
         //     blankObj[obj.fieldPath] ='';
         // });
-
         for (let i = 0; i < dynamicArray.length; i++) {
             const { fieldPath } = dynamicArray[i];
             blankObj[fieldPath] = "";
@@ -272,4 +236,8 @@ export default class AccountDatatable extends LightningElement {
         console.log('this.allData After :', JSON.parse(JSON.stringify(this.allData)));
     }
 
+    getAlldata(data)
+    {
+
+    }
 }
